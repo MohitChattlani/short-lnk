8ab730076690adb17f81334779c6851defa4c704
Mohit Chattlani <chattlanimohit@gmail.com>
2017-07-23 17:30:06 +0530

    Added readme docs

--- imports/ui/components/Login.js
+++ imports/ui/components/Login.js
@@ -38,7 +38,7 @@ export default class Login extends React.Component {
     return (
       <div className="boxed-view">
         <div className="boxed-view__box">
-          <h1>ShortLnk</h1>
+          <h1>ShortLnk Login</h1>
           {this.state.error ? <p>{this.state.error}</p> : undefined}
           <form onSubmit={this.formsubmit.bind(this)} noValidate className="boxed-view__form">
             <input type="email" ref="email" name="email" placeholder="Email"/>
